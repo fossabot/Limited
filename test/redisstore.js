f584efc70553eb042faaca97ff8ed2bf4029fe91
Ethan Waite <ethan@streamjar.tv>
2016-07-26 16:45:55 -0400

    Make Redis much better
    If a server went offline, the requests could linger in the queue and
    stall the others longer than they should have been. This commit sends
    pulses to all servers on startup, and removes any requests for dead
    servers.

--- test/redisstore.js
+++ test/redisstore.js
@@ -34,8 +34,38 @@ describe('Redis Store', () => {
 		store.removeFromQueue('bucket', 1)
 	);
 
+	it('removes dead servers', () =>
+		store.redis.hincrby('ratelimit:buckets:user-register:servers', 'dead', 1).then(() =>
+			store.redis.set('ratelimit:buckets:user-register:pending', 1)
+		)
+		.then(() =>
+			store.sendPulse(1)
+		)
+		.then(() =>
+			store.redis.get('ratelimit:buckets:user-register:pending')
+		)
+		.then(pending => {
+			expect(pending).to.equal('0');
+		})
+	);
+
+	it('answers pulse ping', done => {
+		let called = false;
+		store.pulsing = true;
+		store.knownServers = [];
+		store.sub.on('message', channel => {
+			if (!called && channel === 'ratelimit:servers:pong') {
+				called = true;
+				expect(store.knownServers).to.have.length(1);
+				done();
+			}
+		});
+		store.redis.publish('ratelimit:servers:ping', 'test');
+	});
+
 	it('also accepts ioredis object', () => {
-		const redis = new Redis({ host: '127.0.0.1', port: 6379 });
-		expect(limited({ redis }).store.redis).to.deep.equal(redis);
+		const pub = new Redis({ host: '127.0.0.1', port: 6379 });
+		const sub = new Redis({ host: '127.0.0.1', port: 6379 });
+		expect(limited({ redis: { pub, sub } }).store.redis).to.deep.equal(pub);
 	});
 });
